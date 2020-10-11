d46f3534318f0693ff58556756cb935b936c377b
GeovaneVLG <61693060+GeovaneVLG@users.noreply.github.com>
2020-10-10 21:26:16 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: PagamentoWidget(),
+      home: SplashWidget(),
     );
   }
 }
