a6916b64a4846482224f7b07dc5d565fde6e09d3
GeovaneVLG <geovanevinicius2012@hotmail.com>
2020-10-13 21:26:05 -0300

    Exemplo setState()

--- lib/src/perfil/perfil_widget.dart
+++ lib/src/perfil/perfil_widget.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
 
-class PerfilWidget extends StatelessWidget {
+class PerfilWidget extends StatefulWidget {
+  @override
+  _PerfilWidgetState createState() => _PerfilWidgetState();
+}
+
+class _PerfilWidgetState extends State<PerfilWidget> {
+  var imgUrl;
+  @override
+  void initState() {
+    imgUrl = "assets/images/logo_facebook.png";
+    super.initState();
+  }
+
+  void clicou() {
+    setState(() {
+      if (imgUrl == "assets/images/logo_facebook.png") {
+        imgUrl = "assets/images/logo_ipersonal.jpeg";
+      } else if (imgUrl == "assets/images/logo_ipersonal.jpeg") {
+        imgUrl = "assets/images/logo_google.png";
+      } else if (imgUrl == "assets/images/logo_google.png") {
+        imgUrl = "assets/images/visa.jpg";
+      } else {
+        imgUrl = "assets/images/logo_facebook.png";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +40,19 @@ class PerfilWidget extends StatelessWidget {
         centerTitle: true,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[Text("Teste!!"), Text("Teste2")],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            GestureDetector(
+                onTap: clicou,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(imgUrl),
+                  width: MediaQuery.of(context).size.width * 0.6,
+                ))
+          ],
+        ),
       ),
     );
   }
