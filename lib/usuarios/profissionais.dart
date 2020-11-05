ac05656212043ddcc6327644f26b63cef9f3a120
GeovaneVLG <geovanevinicius2012@hotmail.com>
2020-11-05 18:55:43 -0300

    Atualização classe profissionais

--- lib/usuarios/profissionais.dart
+++ lib/usuarios/profissionais.dart
@@ -1,9 +1,32 @@
 import 'package:ipersonal/usuarios/usuarios.dart';
 
 class Profissionais extends Usuarios {
+  //Atributos da classe Profissional.
+
+  String _profissao;
+  String _formacao;
+
+  //Contrutor da classe.
   Profissionais(String nome, String email, String telefone, String endereco,
       String datanascimento)
       : super(nome, email, telefone, endereco, datanascimento);
+
+  //Metodos SET e GET dos atributos PROFISSAO e FORMACAO.
+  void setProfissao(String profissao) {
+    this._profissao = profissao;
+  }
+
+  String getProfissao() {
+    return this._profissao;
+  }
+
+  void setFormacao(String formacao) {
+    this._formacao = formacao;
+  }
+
+  String getFormacao() {
+    return this._formacao;
+  }
 }
 
 main() {
